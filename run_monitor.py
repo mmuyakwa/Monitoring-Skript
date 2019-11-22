47c87237c20b7cf460641cb78df8f3ddfdb26b18
Michael Muyakwa <34812005+mmuyakwa@users.noreply.github.com>
2019-11-22 23:37:58 +0100

    Stand 23:37 Uhr - Committed at: 2019-11-22 23:37 Uhr

--- run_monitor.py
+++ run_monitor.py
@@ -4,8 +4,8 @@
 # description:   This script is meant for monitoring Linux-Computers.
 # author:        Michael Muyakwa - IT8g
 # created:       2019-11-02
-# updated:       2019-11-21
-# version:       1.4
+# updated:       2019-11-22
+# version:       1.5
 # license:       MIT
 
 # Imports der verwendeten Bibliotheken.
@@ -16,17 +16,17 @@ import psutil  # Um Infos vom System zu erhalten. (Leider nicht 100% zuverlässi
 import configparser  # Spezielle Bibliothek für INI-Files
 import datetime  # Parsen und formatieren vom Datum
 import smtplib  # Zum verschicken der Alarm-Email, wenn Schwellwerte überschritten wurden.
-import email.utils
+import email.utils # Hilfsbibliothek für Emails.
 from email.mime.text import MIMEText # MIMEtypes für Email-Inhalt.
 from email.header import Header # Header für Email.
 
-# Setze Variablen auf, die im Verlauf verwendet werden. (Nur Variablen, die unter jeder Platform funktionieren.)
+# Setze Variablen auf, die im Verlauf verwendet werden. 
+# (Nur Variablen, die unter jeder Platform funktionieren.)
 
 # Pfade zu den verwendeten Dateien.
 pathname = os.path.dirname(os.path.realpath(__file__))  # Pfad vom Ordner in dem das Skript liegt.
 iniFile = os.path.abspath(pathname) + '/settings.ini'  # Pfad zum INI-File
 logFile = os.path.abspath(pathname) + '/log.txt'  # Pfad zum Log-File
-
 # Handler für die INI-Datei.
 config = configparser.ConfigParser()  # Objekt um mit dem INI-File zu arbeiten.
 config.read(iniFile)  # Lese das INI-File ein.
@@ -34,70 +34,73 @@ config.read(iniFile)  # Lese das INI-File ein.
 hostname = platform.node() # Hostname
 
 # Variable für das Logging
-log_str = str(hostname + ': ' + (datetime.datetime.now()).strftime("%Y-%m-%d %H:%M:%S"))
+log_str = str('################\n' + hostname + ': ' + (datetime.datetime.now()).strftime("%Y-%m-%d %H:%M:%S"))
 alarm = False  # Wird auf True gesetzt wenn ein vorgegebener Schwellwert überschritten wurde.
-
 
 # Funktionen
 
-# Log-Funktion
+# Log-Funktion zum schreieben der Log-Datei.
 def writeLog(logStr):
     f = open(logFile, "a+")  # Öffne Log-Datei im Append-Mode
     f.write(logStr)  # Schreibe in die Log-Datei.
     f.write('\n')  # Setze eine leere neue Zeile am Ende der Log-Datei.
     f.close()  # Gebe Zugriff auf das Log-File wieder frei.
 
-# Funktion um sowohl ins Log, wie auch als Ausgabe zu schreiben.
+# Funktion um sowohl ins Log, wie auch die Ausgabe zu schreiben.
 def splitPL(termStr):
-    print(termStr)
-    termStr = str(log_str + '\n' + termStr)
-    writeLog(termStr)
+    print(termStr) # Ausgabe in Condole.
+    termStr = str(log_str + '\n' + termStr) # Setze String für Log-Datei zusammen.
+    writeLog(termStr) # Schreibe Log-Datei.
 
 # Funktion für Linux-Clients.
 def mon_linux():
-    print("This is a Linux-Environment.")
+    print("This is a Linux-Environment.") # Ausgabe, das es sich um eine Linux.Umgebung handelt.
 
     # setze Variablen auf, die im Verlauf verwendet werden (Linux only).
     cpu_p = (round(float(
         os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()),
-        2))
-    v_memory = psutil.virtual_memory()[2]
-    hdd_p = psutil.disk_usage('/')[3]
-    ds = os.statvfs('/')
-    tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
-    num_processes = psutil.Process().children()
+        2)) # CPU-Auslastung in Prozent (Linux only)
+    v_memory = psutil.virtual_memory()[2] # Virtual Memory
+    hdd_p = psutil.disk_usage('/')[3] # Verwendeter Speicherplatz in Prozent.
+    ds = os.statvfs('/') # Hole weiterführende Informationen zum Speicherplatz.
+    tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:]) # Memory (RAM) aufgeteilt.
+    
+    # Informationen zur Festplatte ausgeben.
     disk_str = {"Used": ((ds.f_blocks - ds.f_bfree) * ds.f_frsize) / 10 ** 9,
                 "Unused": (ds.f_bavail * ds.f_frsize) / 10 ** 9}
+    #num_processes = psutil.Process().children() # Gibt keinen zuverlässigen Wert zurück.
 
+    # Generiere Ausgabe für Log und Console-Ausgabe.
     Ausgabe = ('CPU: %s' % cpu_p)
-    Ausgabe += (' - Es laufen %s Prozesse' % num_processes)
+    #Ausgabe += (' - Es laufen %s Prozesse' % num_processes)
     Ausgabe += ('\n%s Ram' % v_memory)
     Ausgabe += ('\n%s' % hdd_p)
     Ausgabe += (' - %s' % disk_str)
-    mem_p = (used_m / tot_m * 100)
-
+    
+    mem_p = (used_m / tot_m * 100) # Memory in Prozent.
     values=psutil.virtual_memory()
-    total_size = get_human_readable_size(values.total)
+    total_size = get_human_readable_size(values.total) # Speichert die gesamt vorhandene RAM-Größe.
     Ausgabe += (' - Virtual Memory: %s' % (total_size))
-
     Ausgabe += (' - Total Mem:%s Used Mem:%s Free Mem:%s - %.2f Prozent in Benutzung.' % (tot_m, used_m, free_m, mem_p))
-    splitPL(Ausgabe)
-    checkAlarm(cpu_p, hdd_p, mem_p)
+    splitPL(Ausgabe) # Ausgabe in Log und Console.
+    checkAlarm(cpu_p, hdd_p, mem_p) # Prüfe ob Schwellenwerte überschritten wurden.
 
 # Funktion zum prüfen ob Schwellwerte überschritten wurden.
 def checkAlarm(cpu_p, hdd_p, mem_p):
-    cpu_p = float(cpu_p)
-    hdd_p = float(hdd_p)
-    mem_p = float(hdd_p)
+    cpu_p = float(cpu_p) # Stelle sicher, das die Variable als Float verarbeitet wird.
+    hdd_p = float(hdd_p) # Stelle sicher, das die Variable als Float verarbeitet wird.
+    mem_p = float(hdd_p) # Stelle sicher, das die Variable als Float verarbeitet wird.
+    # Prüfe ob ein Schwellenwert aus der INI-Datei überschritten wurde.
     if (cpu_p > (float(config['SCHWELLENWERTE']['CPU_P']))) or (hdd_p > (float(config['SCHWELLENWERTE']['HDD_P']))) or (mem_p > (float(config['SCHWELLENWERTE']['MEM_P']))):
         alarm = True
     # Prüfe ob ein Schwellwert überschritten wurde. Falls ja alamiere via Email.
     if alarm:
-        writeLog('Alarm! Ein Schwellwert wurde überschritten.')
-        runAlarm(cpu_p, hdd_p, mem_p); 
+        writeLog('Alarm! Ein Schwellwert wurde überschritten.') # Schreibe in die Log-Datei.
+        runAlarm(cpu_p, hdd_p, mem_p);  # Generiere Alarm-EMAIL.
 
 # Funktion zum alamieren, wenn ein Schwellwert überschritten wurde.
 def runAlarm(cpu_p, hdd_p, mem_p):
+    # Hole nötige Variablen aus der INI-Datei.
     mail_host = config['EMAIL']['Smtp']
     mail_user = config['EMAIL']['Username']
     mail_pass = config['EMAIL']['Password']
@@ -110,6 +113,7 @@ def runAlarm(cpu_p, hdd_p, mem_p):
     message['Subject'] = Header('Es wurde ein Alarm ausgelöst.', 'utf-8')
     port = config['EMAIL']['Port']
 
+    # Baue Email-Objekt auf.
     smtp_obj = smtplib.SMTP(mail_host, port)
     smtp_obj.set_debuglevel(True)
 
@@ -120,19 +124,18 @@ def runAlarm(cpu_p, hdd_p, mem_p):
 
         # Wenn der Server TLS unterstützt, mit TLS-Verschlüsselung weiter machen.
         if smtp_obj.has_extn('STARTTLS'):
-            smtp_obj.starttls()
+            smtp_obj.starttls() # Beginne Verschlüsselung.
             smtp_obj.ehlo() # Erneut Kontakt aufnehmen MIT TLS-Verschlüsselung.
 
-        smtp_obj.login(mail_user, mail_pass)
-        smtp_obj.sendmail(sender, receivers, message.as_string())
-        info_str = 'Alarm-Email wurde verschickt.'
-        print(info_str)
-        writeLog(info_str)
+        smtp_obj.login(mail_user, mail_pass) # SMTP-Zugangsdaten übergeben.
+        smtp_obj.sendmail(sender, receivers, message.as_string()) # Email-Inhalt übergeben.
+        info_str = 'Alarm-Email wurde verschickt.' # String für die Log-Datei.
+        splitPL(info_str) # Ausgabe in Log und Console.
     except smtplib.SMTPException:
         err_str = 'Beim versenden der Email ist ein Fehler aufgetreten.'
-        print(err_str); writeLog(err_str)
+        splitPL(err_str) # Ausgabe in Log und Console.
     finally:
-        smtp_obj.quit()
+        smtp_obj.quit() # Beende SMTP-Verbindung. (Schließe Stream.)
 
 # Wandelt Werte aus PSUtils in brauchbare (gerundete) Größen um.
 def get_human_readable_size(num):
